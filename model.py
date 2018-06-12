75ef96319679ac691a32fa282da2681189e8a1a0
Mitchell Douglass <mrdouglass95@gmail.com>
2018-06-12 03:25:06 -0700

    fix

--- model.py
+++ model.py
@@ -471,9 +471,9 @@ class NeuralModule(torch.nn.Module):
                                  torch.ones(user_ids.shape[0], 1,
                                             device='cuda' if torch.cuda.is_available() else 'cpu')),
                                 dim=1)
-        user_embeddings = self.user_embeddings(
-            torch.max(user_ids, torch.tensor(
-                self.num_users+1, device='cuda' if torch.cuda.is_available() else 'cpu')))
+        user_embeddings = self.user_embeddings(torch.min(user_ids,
+                torch.tensor(self.num_users,
+                             device='cuda' if torch.cuda.is_available() else 'cpu')))
         preds = (user_embeddings * product_sem).sum(dim=1)
         return preds, user_embeddings
 
